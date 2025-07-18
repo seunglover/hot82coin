e4519a4c2096686817e43830ad2e5c6a6cc2e696
seunglover <wjdtmd82@gmail.com>
2025-07-18 12:18:05 +0900

    거래량 텍스트 수정

--- js/app.js
+++ js/app.js
@@ -211,7 +211,7 @@ class CoinRankingApp {
                     <div>KRW 가격</div>
                     <div>거래량</div>
                     <div>변동률</div>
-                    <div>거래량 급등</div>
+                    <div>거래량</div>
                 </div>
                 ${coins.map(coin => this.createCoinItem(coin)).join('')}
             </div>
@@ -265,7 +265,7 @@ class CoinRankingApp {
         
         // 거래량이 많은 코인에도 뱃지 표시 (상위 10개)
         if (coin.rank <= 10) {
-            return '<span class="volume-high-badge">🔥 많음</span>';
+            return '<span class="volume-high-badge">🔥 거래량 급등</span>';
         }
         
         return '';
