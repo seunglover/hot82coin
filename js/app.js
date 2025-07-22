47e6979fa12c3c054040fed04083c5f41a598745
seunglover <wjdtmd82@gmail.com>
2025-07-23 04:50:21 +0900

    sortCoins 메서드의 AI 추천 조건을 3으로 조정

--- js/app.js
+++ js/app.js
@@ -507,7 +507,7 @@ class CoinRankingApp {
                 break;
             case 'ai':
                 // AI 추천: AI 점수 높은 코인만
-                sortedCoins = sortedCoins.filter(coin => coin.aiScore >= 2);
+                sortedCoins = sortedCoins.filter(coin => coin.aiScore >= 3);
                 break;
             default: // 'all'
                 // 전체: 메인코인 우선 표시 (displayFilteredCoins와 동일한 로직)
